5e4df08224df7c0d5fb7da68e48a6c839de55588
beratberkayg <berat.1675@gmail.com>
2023-09-27 16:56:23 +0300

    update

--- random_number/src/App.jsx
+++ random_number/src/App.jsx
@@ -1,6 +1,16 @@
 import { useState } from "react";
 import "./app.css";
 
+const [minVal, setMinVal] = useState("0");
+
+const [maxVal, setMaxVal] = useState("10");
+
+const [randomNum, setRandomNum] = useState("5");
+
+const giveRandomNum = () => {
+  setRandomNum(Math.floor(Math.random() * (maxVal - minVal + 1) + minVal));
+};
+
 function App() {
   return (
     <div className="hero">
@@ -10,14 +20,26 @@ function App() {
           <p>Generate a random number between 1 and 100...</p>
         </div>
         <div className="randomNum">
-          <p>Random Number : {}</p>
+          <p>
+            Random Number : <span>{randomNum}</span>
+          </p>
         </div>
         <div className="inputContainer">
-          <input type="number" aria-valuemin={0} placeholder="Minimum Value" />
+          <input
+            type="number"
+            placeholder="Minimum Value"
+            value={minVal}
+            onChange={(e) => setMinVal(e.target.value)}
+          />
 
-          <input type="number" placeholder="Maximum Value" />
+          <input
+            type="number"
+            placeholder="Maximum Value"
+            value={maxVal}
+            onChange={(e) => setMaxVal(e.target.value)}
+          />
         </div>
-        <button>Get Random Number</button>
+        <button onClick={giveRandomNum}>Get Random Number</button>
       </div>
     </div>
   );

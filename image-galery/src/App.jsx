dbc98f95cbcfdd611c2cab2639b508c511b11ab8
beratberkayg <berat.1675@gmail.com>
2023-09-28 16:17:39 +0300

    update

--- image-galery/src/App.jsx
+++ image-galery/src/App.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
-import Image from "./Image";
+import Images from "./Image";
 import "./App.css";
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(Images[3]);
+
   return (
     <div className="hero">
       <div className="container">
         <div className="title">
           <h1>React Image Gallery</h1>
         </div>
-        <div className="selected">
-          <img src="" alt="" />
-        </div>
+        <img className="selected" src={selectedImg} />
         <div className="imgContainer">
-          {Image.map((img, index) => (
-            <img key={index} src={img} />
+          {Images.map((img, index) => (
+            <img key={index} src={img} onClick={() => setSelectedImg(img)} />
           ))}
         </div>
       </div>
--- image-galery/src/Image.js
+++ image-galery/src/Image.js
@@ -1,4 +1,4 @@
-const Image = [
+const Images = [
   "https://cdn.photoswipe.com/photoswipe-demo-images/photos/home-demo/luca-bravo-ny6qxqv_m04-unsplash_snrzpf/luca-bravo-ny6qxqv_m04-unsplash_snrzpf_c_scale,w_2500.jpg",
   "https://cdn.photoswipe.com/photoswipe-demo-images/photos/home-demo/luca-bravo-O453M2Liufs-unsplash_qqt53u/luca-bravo-O453M2Liufs-unsplash_qqt53u_c_scale,w_2500.jpg",
   "https://cdn.photoswipe.com/photoswipe-demo-images/photos/home-demo/luca-bravo-VowIFDxogG4-unsplash_ibrktu/luca-bravo-VowIFDxogG4-unsplash_ibrktu_c_scale,w_2500.jpg",
@@ -6,4 +6,4 @@ const Image = [
   "https://cdn.photoswipe.com/photoswipe-demo-images/photos/home-demo/luca-bravo-A-fubu9QJxE-unsplash_jxy5p8/luca-bravo-A-fubu9QJxE-unsplash_jxy5p8_c_scale,w_2500.jpg",
 ];
 
-export default Image;
+export default Images;

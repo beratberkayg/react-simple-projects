33e18c357db4ce62aeff671398958a31f0417257
beratberkayg <berat.1675@gmail.com>
2023-09-29 14:29:41 +0300

    update

--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -3,7 +3,27 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addTodo() {}
+
+  return (
+    <div className="hero">
+      <div className="container">
+        <h1>React To Do List</h1>
+        <input
+          type="text"
+          placeholder="add to do"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button type="submit" onClick={addTodo()}>
+          Add To Do
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;

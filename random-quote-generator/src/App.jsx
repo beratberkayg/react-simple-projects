d8ca70df89e52ba51c7d1e09f5a8fe51d63ff9d7
beratberkayg <berat.1675@gmail.com>
2023-10-01 14:43:35 +0300

    update app.jsx

--- random-quote-generator/src/App.jsx
+++ random-quote-generator/src/App.jsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [quotes, setQuotes] = useState("");
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomNum]);
+      });
+  };
+
+  return (
+    <div className="App">
+      {quotes.text}
+      {quotes.author}
+      <button onClick={getQuote}>Random Quotes</button>
+    </div>
+  );
 }
 
 export default App;

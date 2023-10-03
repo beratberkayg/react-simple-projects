b1bed621960dbef9b03d9cffff0e0c13304eb36e
beratberkayg <berat.1675@gmail.com>
2023-10-03 16:30:46 +0300

    update

--- search-filter/src/App.jsx
+++ search-filter/src/App.jsx
@@ -1,9 +1,41 @@
 import { useState } from "react";
-
+import Data from "./Data.json";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <div className="hero">
+      <h1>Kurtlar Vadisi Konsey Üyeleri</h1>
+      <input
+        type="text"
+        placeholder="Konsey Üyesi Giriniz.."
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div className="data-container">
+        {Data.filter((item) => {
+          if (searchTerm == "") {
+            return item;
+          } else if (
+            item.title
+              .toLocaleLowerCase()
+              .includes(searchTerm.toLocaleLowerCase())
+          ) {
+            return item;
+          }
+        }).map((item) => {
+          return (
+            <div className="card" key={item.id}>
+              <img src={item.image} />
+              <h3>{item.title}</h3>
+              <p>{item.Description}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;

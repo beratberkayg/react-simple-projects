41acc8e0c7a1b13d8832685de6fe1422dc892fad
beratberkayg <berat.1675@gmail.com>
2023-10-07 22:57:04 +0300

    update app.jsx

--- random-color/src/App.jsx
+++ random-color/src/App.jsx
@@ -7,9 +7,12 @@ function App() {
   
   function randomHex(){
     const randomColor = "#"+ Math.floor(Math.random()*16777721).toString(16);
+    setHex(randomColor);
   }
   return <div className="App">
-
+    <h1>{hex}</h1>
+    <button>Click For New Background Color</button>
+    <button>Copy The Hex Value</button>
   </div>;
 }
 

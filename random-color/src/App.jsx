cf506caf959ad4bb3a8ac36e4e5c0e54d02912f3
beratberkayg <berat.1675@gmail.com>
2023-10-07 22:54:47 +0300

    update app.jsx

--- random-color/src/App.jsx
+++ random-color/src/App.jsx
@@ -3,7 +3,14 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [hex, setHex] = useState(#ffffff)
+  
+  function randomHex(){
+    const randomColor = "#"+ Math.floor(Math.random()*16777721).toString(16);
+  }
+  return <div className="App">
+
+  </div>;
 }
 
 export default App;

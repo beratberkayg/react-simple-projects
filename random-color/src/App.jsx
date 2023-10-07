88f59e297b9a66682397447e7ef32c4ec9f37ed9
beratberkayg <berat.1675@gmail.com>
2023-10-07 23:03:31 +0300

    update app.css

--- random-color/src/App.jsx
+++ random-color/src/App.jsx
@@ -3,17 +3,21 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [hex, setHex] = useState(#ffffff)
-  
-  function randomHex(){
-    const randomColor = "#"+ Math.floor(Math.random()*16777721).toString(16);
+  const [hex, setHex] = useState("#ffffff");
+
+  function randomHex() {
+    const randomColor = "#" + Math.floor(Math.random() * 16777721).toString(16);
     setHex(randomColor);
   }
-  return <div className="App">
-    <h1>{hex}</h1>
-    <button onClick={randomHex}>Click For New Background Color</button>
-    <button onClick={()=> navigator.clipboard.writeText(hex)}>Copy The Hex Value</button>
-  </div>;
+  return (
+    <div className="App">
+      <h1>{hex}</h1>
+      <button onClick={randomHex}>Click For New Background Color</button>
+      <button onClick={() => navigator.clipboard.writeText(hex)}>
+        Copy The Hex Value
+      </button>
+    </div>
+  );
 }
 
 export default App;

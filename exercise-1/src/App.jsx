619bbec126ba68bdbf1cdfa582434dbba11220dd
beratberkayg <berat.1675@gmail.com>
2023-10-09 17:11:27 +0300

    update

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ exercise-1/src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Data = [
+  {
+    id: 1,
+    name: "Berat Berkay",
+    surname: "Gökdemir",
+    img: "https://media.licdn.com/dms/image/D4D03AQFlHNJMfz2O1w/profile-displayphoto-shrink_800_800/0/1682802966671?e=2147483647&v=beta&t=s36c66gJBxPn5l4MAjFgTmp-jAkVjgWvqrzhHTKjhtE",
+  },
+  {
+    id: 2,
+    name: "Hasan Hüseyin",
+    surname: "Köse",
+    img: "https://yucelborufenlisesi.meb.k12.tr/meb_iys_dosyalar/41/02/750301/resimler/2021_06/10134136_hk.jpg",
+  },
+  {
+    id: 3,
+    name: "Yunus Emre",
+    surname: "Akdemir",
+    img: "https://contents.mediadecathlon.com/p2218125/k$c3b4c0749e70b67c2b36c99316449d9d/sq/8602698.jpg?format=auto&f=800x0",
+  },
+];
+
+function Home() {
+  return (
+    <div className="Home">
+      {Data.map((dt, i) => {
+        return (
+          <div className="card" key={i}>
+            <img src={dt.img} />
+            <h2>{dt.name}</h2>
+            <h3>{dt.surname}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Home;

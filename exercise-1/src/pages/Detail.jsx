cf87529afb67691cfe836d752d8d93b4556fbb38
beratberkayg <berat.1675@gmail.com>
2023-10-09 19:45:03 +0300

    update

--- exercise-1/src/pages/Detail.jsx
+++ exercise-1/src/pages/Detail.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import Home, { Data } from "./Home";
+import { Data } from "./Home";
 
 function Detail() {
   const { id } = useParams();

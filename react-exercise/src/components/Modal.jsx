9628961f043c7d4eb281fa96fdfd245ae040d340
beratberkayg <berat.1675@gmail.com>
2023-10-16 14:29:16 +0300

    update

--- react-exercise/src/components/Modal.jsx
+++ react-exercise/src/components/Modal.jsx
@@ -1,42 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import { GrClose } from "react-icons/gr";
-import Input from "./Input";
+import { useDispatch } from "react-redux";
+import { modalFunc } from "../redux/modalSlice";
 
-const Modal = ({ title, concent, btnText, btnFunc }) => {
-  const [productInfo, setProductInfo] = useState({
-    name: "",
-    price: "",
-    url: "",
-  });
-
+const Modal = ({ title, content }) => {
+  const dispatch = useDispatch();
   return (
     <div className="fixed top-0 left-0 bottom-0 right-0 w-full h-screen flex items-center justify-center">
       <div className="w-1/3 bg-white shadow-lg rounded-md p-4">
         <div className="border-b py-3 flex items-center justify-between">
-          <div className="text-2xl">{title}</div>
-          <GrClose size={24} />
+          <div className="text-2xl ">{title}</div>
+          <GrClose
+            className="cursor-pointer"
+            onClick={() => dispatch(modalFunc())}
+            size={24}
+          />
         </div>
-        <Input
-          type={"text"}
-          placeholder={"Ürün Ekle"}
-          name={"name"}
-          id={"name"}
-          onChange={(e) => onChangeFunc(e, "name")}
-        />
-        <Input
-          type={"text"}
-          placeholder={"Fiyat Ekle"}
-          name={"price"}
-          id={"price"}
-          onChange={(e) => onChangeFunc(e, "price")}
-        />
-        <Input
-          type={"file"}
-          placeholder={"Resim Seç"}
-          name={"url"}
-          id={"url"}
-          onChange={(e) => onChangeFunc(e, "url")}
-        />
+        {content}
       </div>
     </div>
   );
--- react-exercise/src/pages/Product.jsx
+++ react-exercise/src/pages/Product.jsx
@@ -1,22 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductCard from "../components/ProductCard";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Modal from "../components/Modal";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { createDataFunc } from "../redux/dataSlice";
+import { modalFunc } from "../redux/modalSlice";
 
 function Product() {
   const { modal } = useSelector((state) => state.modal);
-  console.log(modal, "modal");
-  const buttonFunc = () => {};
+  const { data } = useSelector((state) => state.data);
+  const dispatch = useDispatch();
+
+  const [productInfo, setProductInfo] = useState({
+    name: "",
+    price: "",
+    url: "",
+  });
+
+  const onChangeFunc = (e, type) => {
+    if (type == "url") {
+      setProductInfo((prev) => ({
+        ...prev,
+        [e.target.name]: URL.createObjectURL(e.target.files[0]),
+      }));
+    } else {
+      setProductInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    }
+  };
+
+  const buttonFunc = () => {
+    dispatch(createDataFunc());
+    dispatch(modalFunc());
+  };
+
+  const contentModal = (
+    <>
+      <Input
+        type={"text"}
+        placeholder={"Ürün Ekle"}
+        name={"name"}
+        id={"name"}
+        onChange={(e) => onChangeFunc(e, "name")}
+      />
+      <Input
+        type={"text"}
+        placeholder={"Fiyat Ekle"}
+        name={"price"}
+        id={"price"}
+        onChange={(e) => onChangeFunc(e, "price")}
+      />
+      <Input
+        type={"file"}
+        placeholder={"Resim Seç"}
+        name={"url"}
+        id={"url"}
+        onChange={(e) => onChangeFunc(e, "url")}
+      />
+      <Button btnText={"Ürün Oluştur"} onClick={buttonFunc} />
+    </>
+  );
+
   return (
     <div>
       <ProductCard />
-      {modal && (
-        <Modal
-          title={"Ürün Oluştur"}
-          btnText={"Oluştur"}
-          btnFunc={buttonFunc}
-        />
-      )}
+      {modal && <Modal content={contentModal} title={"Ürün Oluştur"} />}
     </div>
   );
 }

c8e9d8d3e9115798aa320d39948589d6f9f913ba
beratberkayg <berat.1675@gmail.com>
2023-10-24 18:08:22 +0300

    next/link

--- next.js/app/page.jsx
+++ next.js/app/page.jsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
       Merhaba
+      <Link href="/hakkimizda">Hakkımızda</Link>
+      <Link href="/docs/post-1">Post 1</Link>
+      <Link href="/docs/post-1/post-2">Post 2</Link>
+      <Link
+        className="text-2xl"
+        href={{
+          pathname: "/hakkimizda",
+          query: {
+            name: "Next.js",
+          },
+        }}
+      >
+        Hakkımızda Obje İle
+      </Link>
     </main>
   );
 }
